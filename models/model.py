53a77ccabc80fa99a01cf7270dc4f169eda6922f
ChristianHou <775349292@qq.com>
2024-08-01 17:21:16 +0800

    修改reqs

--- models/model.py
+++ models/model.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 
 
 class SourceRequest(BaseModel):
-    client_ip:str
+    client_ip: str | None = None
     sentences: str | None = None
     source_lang: str = None
     target_lang: str = None
